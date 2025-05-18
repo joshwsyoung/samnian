cf2b4143ccaa58cca8474ca399f86f0e11b846b1
Josh Young <103465268+QueensgateCreative@users.noreply.github.com>
2025-05-18 01:36:28 +0100

    Got mad

--- partials/header.php
+++ partials/header.php
@@ -20,7 +20,7 @@ if (session_status() === PHP_SESSION_NONE) session_start();
     
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/styles.css">
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     
